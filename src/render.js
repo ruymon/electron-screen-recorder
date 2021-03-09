da416584e463e249e626d20b6a35fbc1d7ab5319
ruymon <ruystfrancis@gmail.com>
2021-03-09 17:31:01 -0300

    first commit

---
+++ src/render.js
@@ -0,0 +1,54 @@
+//Buttons
+const videoElement = document.querySelector('video');
+const startBtn = document.getElementById('startBtn');
+const stopBtn = document.getElementById('stopBtn');
+const videoSelectBtn = document.getElementById('videoSelectBtn');
+videoSelectBtn.onclick = getVideoSources;
+
+const { desktopCapturer, remote } = require('electron');
+const { Menu } = remote;
+
+// Get the available video sources
+async function getVideoSources() {
+    const inputSources = await desktopCapturer.getSources({
+        types: ['window', 'screen']
+    });
+
+    const videoOptionsMenu = Menu.buildFromTemplate(
+        inputSources.map(source => {
+            return {
+                label: source.name,
+                click: () => selectSource(source)
+            };
+        })
+    );
+
+
+    videoOptionsMenu.popup();
+}
+
+// Change the videoSource window to Record
+async function selectSource(source) {
+
+    videoSelectBtn.innerHTML = source.name;
+    
+    const constraints = {
+        audio: false,
+        video: {
+            mandatory: {
+                chromeMediaSource: 'desktop',
+                chromeMediaSourceId: source.id
+            }
+        }
+    };
+
+    // Create a stream
+    const stream = await navigator.mediaDevices
+        .getUserMedia(constraints);
+
+    // Preview the Source in the HTML Video Element
+    videoElement.srcObject = stream;
+    videoElement.play();
+
+
+}
